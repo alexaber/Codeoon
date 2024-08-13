c2b5247ba17e37270c45141cf714781dd08c42a7
alexaber <105004302+alexaber@users.noreply.github.com>
2024-08-13 17:13:47 +0300

    Update test.js

--- Codeoon/js/test.js
+++ Codeoon/js/test.js
@@ -1,1 +1,13 @@
+</html>
+function include(url) {
+	let script = document.createElement('script');
+	script.src = url;
+	script.async = false;
+	document.getElementsByTagName('head')[0].appendChild(script);
+function include(url) {
+	let script = document.createElement('script');
+	script.src = url;
+	script.async = false;
+	document.getElementsByTagName('head')[0].appendChild(script);
+</html>
 ﻿
